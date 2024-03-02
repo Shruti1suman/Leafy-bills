297c04c3b0cb5b7220c251e5ec03c7dbd7bd7518
MaryamMohamedYahya <ummabdullahyahya@gmail.com>
2024-03-03 01:17:30 +0530

    sw

--- invoice.js
+++ invoice.js
@@ -60,7 +60,7 @@ document.getElementById('downloadPdf').addEventListener('click', function() {
     const options = {
 
         filename: 'Leafy Bills.pdf',
-        html2canvas: {},
+        html2canvas: { scale: 2 },
         jsPDF: { unit: 'in', format: 'ISO A4', orientation: 'portrait' }
       };
 
