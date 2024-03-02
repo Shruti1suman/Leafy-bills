76b2109792c76ab8343f7b363b3c874daeff9d86
MaryamMohamedYahya <ummabdullahyahya@gmail.com>
2024-03-03 00:59:42 +0530

    uuu

--- invoice.js
+++ invoice.js
@@ -57,16 +57,9 @@ delRow =(el)=> {
 
 document.getElementById('downloadPdf').addEventListener('click', function() {
     const invoiceElement = document.getElementById('invoice');
-    const options = {
-      margin: 1,
-      filename: 'invoice.pdf',
-      image: { type: 'jpeg', quality: 0.98 },
-      html2canvas: { scale: 2 },
-      jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
-    };
+    
 
     // Then call html2pdf with the element and options
     html2pdf().from(invoiceElement).set(options).save();
   });
 
-  
